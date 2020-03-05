100f84e99ca3961d1e8ff326abc0320a9e1cccfa
Gvan-Iovnov <60030514+Gvan-Iovnov@users.noreply.github.com>
2020-03-05 21:06:23 +0300

    Major changes
    Made the page able to survive refreshes
    Changed the way the script finds the winner
    Undo/Redo functions are on their way

--- index.js
+++ index.js
@@ -1,86 +1,114 @@
-let bool = 1;
-field.onclick = function(event){
-	let target = event.target;
-	// console.log(target);
-	// console.log(target.classList);
-	// console.log(target.classList.contains('cell'));
-	if(target.classList.contains('cell')){
-		(bool) ? target.classList.add("ch") : target.classList.add("r");
-		(bool) ? checkField(target, 'ch') : checkField(target, 'r');
-		bool = !bool;
+let fieldobj = document.querySelectorAll('[data-id]');
+let storage = localStorage;
+let move=0;	
+// storage.setItem('moves', '');
+window.onload = function(){
+	if(storage.getItem('moves')){
+		let arrOfMoves = storage.getItem('moves').split(' ');
+		for(let i=1; i<arrOfMoves.length; i++){
+			let arr = arrOfMoves[i].split(',');
+			doMove(arr[0], arr[1])
+		}
+	}else{
+		storage.setItem('current', '1');
 	}
 }
-function checkField(el, cellClass){
-	id = parseInt(el.getAttribute('data-id'), 10);
-	let fieldobj = document.querySelectorAll('[data-id]');
-	// console.log(fieldobj);
-	// console.log(fieldobj[id].classList.contains('ch'));
-	// console.log(fieldobj[id-COLS_COUNT]);
-	// COLUMS 
-	if(fieldobj[id-COLS_COUNT*2]){
-		if(fieldobj[id-COLS_COUNT].classList.contains(cellClass) && fieldobj[id-COLS_COUNT*2].classList.contains(cellClass)) 
-			declarewinner(cellClass, 'vertical', [fieldobj[id], fieldobj[id-COLS_COUNT], fieldobj[id-COLS_COUNT*2]]);
-	}
-	if(fieldobj[id+COLS_COUNT*2]){
-		if(fieldobj[id+COLS_COUNT].classList.contains(cellClass) && fieldobj[id+COLS_COUNT*2].classList.contains(cellClass))
-			declarewinner(cellClass, 'vertical', [fieldobj[id], fieldobj[id+COLS_COUNT], fieldobj[id+COLS_COUNT*2]]);
-	}
-	if(fieldobj[id+COLS_COUNT] && fieldobj[id-COLS_COUNT]){
-		if(fieldobj[id-COLS_COUNT].classList.contains(cellClass) && fieldobj[id+COLS_COUNT].classList.contains(cellClass))
-			declarewinner(cellClass, 'vertical', [fieldobj[id], fieldobj[id+COLS_COUNT], fieldobj[id-COLS_COUNT]]);
-	}
-	// ROWS
-	if(id%ROWS_COUNT>1){
-		if(fieldobj[id-1].classList.contains(cellClass) && fieldobj[id-2].classList.contains(cellClass))
-			declarewinner(cellClass, 'horizontal', [fieldobj[id], fieldobj[id-1], fieldobj[id-2]]);
-	}
-	if(id%ROWS_COUNT<COLS_COUNT-2){
-		if(fieldobj[id+1].classList.contains(cellClass) && fieldobj[id+2].classList.contains(cellClass))
-			declarewinner(cellClass, 'horizontal', [fieldobj[id], fieldobj[id+1], fieldobj[id+2]]);
-	}
-	if(id%ROWS_COUNT>0 && id%ROWS_COUNT<COLS_COUNT-1){
-		if(fieldobj[id-1].classList.contains(cellClass) && fieldobj[id+1].classList.contains(cellClass))
-			declarewinner(cellClass, 'horizontal', [fieldobj[id], fieldobj[id-1], fieldobj[id+1]]);
-	}
-	//LEFT DIAGONAL
-	if(fieldobj[id-COLS_COUNT*2-2]){
-		if(fieldobj[id-COLS_COUNT-1].classList.contains(cellClass) && fieldobj[id-COLS_COUNT*2-2].classList.contains(cellClass)) 
-			declarewinner(cellClass, 'diagonal-left', [fieldobj[id], fieldobj[id-COLS_COUNT-1], fieldobj[id-COLS_COUNT*2-2]]);
-	}
-	if(fieldobj[id+COLS_COUNT*2+2]){
-		if(fieldobj[id+COLS_COUNT+1].classList.contains(cellClass) && fieldobj[id+COLS_COUNT*2+2].classList.contains(cellClass))
-			declarewinner(cellClass, 'diagonal-left', [fieldobj[id], fieldobj[id+COLS_COUNT+1], fieldobj[id+COLS_COUNT*2+2]]);
-	}
-	if(fieldobj[id-COLS_COUNT-1] && fieldobj[id+COLS_COUNT+1]){
-		if(fieldobj[id-COLS_COUNT-1].classList.contains(cellClass) && fieldobj[id+COLS_COUNT+1].classList.contains(cellClass)) 
-			declarewinner(cellClass, 'diagonal-left', [fieldobj[id], fieldobj[id-COLS_COUNT-1], fieldobj[id+COLS_COUNT+1]]);
-	}
-	//RIGHT DIAGONAL
-	if(fieldobj[id-COLS_COUNT*2+2]){
-		if(fieldobj[id-COLS_COUNT+1].classList.contains(cellClass) && fieldobj[id-COLS_COUNT*2+2].classList.contains(cellClass))
-			declarewinner(cellClass, 'diagonal-right', [fieldobj[id], fieldobj[id-COLS_COUNT+1], fieldobj[id-COLS_COUNT*2+2]]);
-	}
-	if(fieldobj[id+COLS_COUNT*2-2]){
-		if(fieldobj[id+COLS_COUNT-1].classList.contains(cellClass) && fieldobj[id+COLS_COUNT*2-2].classList.contains(cellClass))
-			declarewinner(cellClass, 'diagonal-right', [fieldobj[id], fieldobj[id+COLS_COUNT-1], fieldobj[id+COLS_COUNT*2-2]]);
-	}
-	if(fieldobj[id-COLS_COUNT+1] && fieldobj[id+COLS_COUNT-1]){
-		if(fieldobj[id-COLS_COUNT+1].classList.contains(cellClass) && fieldobj[id+COLS_COUNT-1].classList.contains(cellClass)) 
-			declarewinner(cellClass, 'diagonal-right', [fieldobj[id], fieldobj[id-COLS_COUNT+1], fieldobj[id+COLS_COUNT-1]]);
-	}
-	//CHECK IF DRAW
-	let draw = true;
+field.onclick = function(event) {
+	let bool = +storage.getItem('current');
+    let target = event.target;
+    if (target.classList.contains('cell')) {
+        (bool) ? target.classList.add("ch"): target.classList.add("r");
+        (bool) ? checkField(target, 'ch'): checkField(target, 'r');
+        (bool) ? storage.setItem('current', 0) :  storage.setItem('current', 1);
+    }
+}
+document.querySelector('.restart-btn').onclick = function(){
+	storage.removeItem('moves');
+	storage.setItem('current', 1);	
+	document.querySelector('.won-title').classList.add("hidden");
+	document.querySelector('.won-message').innerHTML = '';
 	for(let i=0; i<fieldobj.length; i++){
-		if(!fieldobj[i].classList.contains('ch') && !fieldobj[i].classList.contains('r')) draw=false;
+		fieldobj[i].className='';
+		fieldobj[i].classList.add('cell')
 	}
-	if(draw && document.querySelector('.won-message').innerHTML.length==0) declarewinner('draw', '', '');
+} 
+function checkField(el, cellClass) {
+    let id = parseInt(el.getAttribute('data-id'), 10);
+    let win = true;
+    let draw = true;
+    let winCells = [];
+    storage.setItem('moves', storage.getItem('moves')+` ${id},${cellClass}`);
+	move++;
+	console.log(storage.getItem('moves').split(' '));
+    // console.log(fieldobj);
+    // console.log(fieldobj[id].classList.contains('ch'));
+    // console.log(fieldobj[id-COLS_COUNT]);
+    // COLUMN
+    let col = id % ROWS_COUNT;
+    for (let i = col; i < fieldobj.length; i += COLS_COUNT) {
+        if (!fieldobj[i].classList.contains(cellClass)) win = false
+    }
+    if (win) {
+        for (let i = col; i < fieldobj.length; i += COLS_COUNT) {
+            winCells.push(fieldobj[i]);
+        }
+        declarewinner(cellClass, 'vertical', winCells);
+    }
+    // ROWS
+    win = true;
+    let row = Math.floor(id / COLS_COUNT);
+    for (let i = 0; i < COLS_COUNT; i++) {
+        if (!fieldobj[i + ROWS_COUNT * row].classList.contains(cellClass)) win = false;
+    }
+    if (win) {
+        for (let i = 0; i < COLS_COUNT; i++) {
+            winCells.push(fieldobj[i + ROWS_COUNT * row]);
+        }
+        declarewinner(cellClass, 'horizontal', winCells);
+    }
+    //RIGHT DIAGONAL
+    if (row == col) {
+        win = true;
+        for (let i = 0; i < fieldobj.length; i += COLS_COUNT + 1) {
+            if (!fieldobj[i].classList.contains(cellClass)) win = false;
+        }
+        if (win) {
+            for (let i = 0; i < fieldobj.length; i += COLS_COUNT + 1) {
+                winCells.push(fieldobj[i]);
+            }
+            declarewinner(cellClass, 'diagonal-right', winCells);
+        }
+    }
+    //LEFT DIAGONAL
+    win=true;
+    for(let i = COLS_COUNT-1; i < fieldobj.length-COLS_COUNT+1; i+=COLS_COUNT-1){
+    	if (!fieldobj[i].classList.contains(cellClass)) win = false;
+    }
+    if (win){
+    	for (let i = COLS_COUNT-1; i < fieldobj.length-COLS_COUNT+1; i+=COLS_COUNT-1) {
+                winCells.push(fieldobj[i]);
+            }
+        declarewinner(cellClass, 'diagonal-left', winCells);
+    }
+    //CHECK IF DRAW
+    for (let i = 0; i < fieldobj.length; i++) {
+        if (!fieldobj[i].classList.contains('ch') && !fieldobj[i].classList.contains('r')) draw = false;
+    }
+    if (draw && document.querySelector('.won-message').innerHTML.length == 0) declarewinner('draw', '', '');
 }
-function declarewinner(cellClass, vector, cells){
-	if(cellClass=='draw') document.querySelector('.won-message').innerHTML = 'It\'s a draw!';
-	document.querySelector('.won-message').innerHTML = (cellClass == 'ch') ? 'Crosses won' : 'Rounds won';
-	document.querySelector('.won-title').classList.remove("hidden");
-	for(let i=0; i<cells.length; i++){
-		cells[i].classList.add(vector);
-		console.log(cells[i])
-	}
+
+function declarewinner(cellClass, vector, cells) {
+    if (cellClass == 'draw') document.querySelector('.won-message').innerHTML = 'It\'s a draw!';
+    document.querySelector('.won-message').innerHTML = (cellClass == 'ch') ? 'Crosses won' : 'Rounds won';
+    document.querySelector('.won-title').classList.remove("hidden");
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].classList.add('win');
+        cells[i].classList.add(vector);
+    }
+}
+function doMove(id, cellClass){
+	document.querySelector(`#c-${id}`).classList.add(cellClass);
+}
+function unDoMove(id, cellClass){
+	document.querySelector(`#c-${id}`).classList.remove(cellClass);
 }
